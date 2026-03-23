2118f1bedebcadc391bc6e9c16275a3a9850a557
Sneha_ydv <snehayadav92456@gmail.com>
2026-03-23 10:45:00 +0530

    added full project

---
+++ script.js
@@ -0,0 +1,7 @@
+const bar = document.getElementById('bar');
+const close=document.getElementById('navbar');
+if(bar){    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+    })
+}
+if(close){}
